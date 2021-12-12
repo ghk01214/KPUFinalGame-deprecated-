b20238dc6df150a6e0ceeb82d64398bb1f6a6438
ghk01214 <ghk01214@naver.com>
2021-12-12 18:27:38 +0900

    서버 기본 파일 생성

---
+++ ShootToLive/Server/pch.h
@@ -0,0 +1,15 @@
+#ifndef _PCH_H_
+#define _PCH_H_
+
+// pch.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+//
+
+// C++ 런타임 헤더 파일입니다.
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+#endif // !_PCH_H_
