82f02e6f23cd4c6355e05f5bda226266002e3e77
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2022-01-21 14:16:47 +0900

    게임 프로세스를 실행하는 게임 서버 생성

---
+++ source/Server/GameServer.cpp
@@ -0,0 +1,2 @@
+﻿#include "GameServer.h"
+
---
+++ source/Server/GameServer.h
@@ -0,0 +1,16 @@
+﻿#ifndef _GAME_SERVER_H_
+#define _GAME_SERVER_H_
+
+#include "NetworkServer.h"
+
+class CGameServer : public CNetworkServer
+{
+public:
+	CGameServer() {}
+	virtual ~CGameServer() = 0;
+
+private:
+
+};
+
+#endif // !_GAME_SERVER_H_
