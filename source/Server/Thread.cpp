c3cdea213de07b156aa6ce3e5b7f217b72c1f602
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2022-01-21 14:17:00 +0900

    네트워크 서버 스레드 생성

---
+++ source/Server/Thread.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+
+DWORD WINAPI AcceptThread(LPVOID arg)
+{
+
+}
+
+DWORD WINAPI WorkerThread(LPVOID arg)
+{
+	std::unique_ptr<SOCKET> sock{ reinterpret_cast<SOCKET*>(arg) };
+
+
+}
