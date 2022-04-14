dc6d25554fc82a0c5d37bd191d0705d2a2340a70
unknown <gsw0305@naver.com>
2022-04-14 15:09:38 +0900

    화면 크기 수정

--- Sample 수정/stdafx.h
+++ Sample 수정/stdafx.h
@@ -42,8 +42,8 @@ using namespace DirectX::PackedVector;
 
 using Microsoft::WRL::ComPtr;
 
-#define FRAME_BUFFER_WIDTH		640
-#define FRAME_BUFFER_HEIGHT		480
+#define FRAME_BUFFER_WIDTH		1080	
+#define FRAME_BUFFER_HEIGHT		600
 
 #define MAX_LIGHTS				8 
 #define MAX_MATERIALS			8 
