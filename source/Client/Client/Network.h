6b78434eaecfc026e0d22bc77611f27a46baee9a
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2022-05-02 08:02:00 +0900

    Overlapped I/O 부분 주석 처리

--- source/Client/Client/Network.h
+++ source/Client/Client/Network.h
@@ -41,6 +41,51 @@ public:
 	char type;
 };
 
+//class CNetwork
+//{
+//public:
+//	CNetwork() = default;
+//	CNetwork(CGameFramework* game_inst);
+//	~CNetwork();
+//
+//	void ConnectToServer();
+//	void Run();
+//
+//	void RecvData();
+//	void SendData();
+//
+//	void SendLoginPacket();
+//	void SendMovePlayerPacket(DWORD direct);
+//
+//	static void CALLBACK RecvCallback(DWORD error, DWORD bytes, LPWSAOVERLAPPED over, DWORD flag);
+//	static void CALLBACK SendCallback(DWORD error, DWORD bytes, LPWSAOVERLAPPED over, DWORD flag);
+//
+//	void ProcessPacket();
+//
+//private:
+//	SOCKET server;
+//
+//	NetworkData recv;
+//	NetworkData send;
+//
+//	int remain_size;
+//
+//	SC::PACKET::LOGIN* recv_login_packet;
+//	SC::PACKET::ADD_PLAYER* recv_add_player_packet;
+//	SC::PACKET::MOVE_PLAYER* recv_move_player_packet;
+//	SC::PACKET::REMOVE_PLAYER* recv_remove_player_packet;
+//
+//	CS::PACKET::LOGIN send_login_packet;
+//	CS::PACKET::MOVE_PLAYER send_move_player_packet;
+//
+//	static std::unique_ptr<CNetwork> instance;
+//	//std::shared_ptr<CGameFramework> game_instance;
+//	CGameFramework* game_instance;
+//
+//	int a_send;
+//	int a_recv;
+//};
+
 class CNetwork
 {
 public:
