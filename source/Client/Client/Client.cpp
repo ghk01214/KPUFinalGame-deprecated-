36e0263e3f86a3f6c8bf6edbcddbfe00d0464116
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2022-05-09 05:03:47 +0900

    클라이언트 접속 시 서버 주소 입력

--- source/Client/Client/Client.cpp
+++ source/Client/Client/Client.cpp
@@ -108,8 +108,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
 	case WM_CREATE:
 	{
-		//DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, DlgProc);
-		SERVER_ADDR = L"127.0.0.1";
+		DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, DlgProc);
+		//SERVER_ADDR = L"127.0.0.1";
 		gGameFramework.ConnectToServer();
 		gGameFramework.Login();
 		gGameFramework.OnCreate(hInst, hWnd);
